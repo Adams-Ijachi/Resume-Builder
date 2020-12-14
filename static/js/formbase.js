6ca54589531c21ab589b88e1b5e2d1fed6d88ef7
Adams Ijachi <adamsijachi9@gmail.com>
2020-12-14 19:16:59 +0100

    minor change

---
+++ static/js/formbase.js
@@ -0,0 +1,124 @@
+$(document).ready(function(){
+
+    //jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+$(".next").click(function(){
+   // validate each page before submit
+    $('#msform').validate();
+    if (!$('#msform').valid()) {
+        
+        return false;
+    }
+   
+    if(animating) return false;
+    animating = true;
+    
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+    
+    //activate next step on progressbar using the index of next_fs
+    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+    
+    //show the next fieldset
+    next_fs.show(); 
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+        step: function(now, mx) {
+            //as the opacity of current_fs reduces to 0 - stored in "now"
+            //1. scale current_fs down to 80%
+            scale = 1 - (1 - now) * 0.2;
+            //2. bring next_fs from the right(50%)
+            left = (now * 50)+"%";
+            //3. increase opacity of next_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({
+        'transform': 'scale('+scale+')',
+        'position': 'absolute'
+    });
+            next_fs.css({'left': left, 'opacity': opacity});
+        }, 
+        duration: 800, 
+        complete: function(){
+            current_fs.hide();
+            animating = false;
+        }, 
+        //this comes from the custom easing plugin
+        easing: 'easeInOutBack'
+    });
+});
+
+$(".previous").click(function(){
+    if(animating) return false;
+    animating = true;
+    
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+    
+    //de-activate current step on progressbar
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+    
+    //show the previous fieldset
+    previous_fs.show(); 
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+        step: function(now, mx) {
+            //as the opacity of current_fs reduces to 0 - stored in "now"
+            //1. scale previous_fs from 80% to 100%
+            scale = 0.8 + (1 - now) * 0.2;
+            //2. take current_fs to the right(50%) - from 0%
+            left = ((1-now) * 50)+"%";
+            //3. increase opacity of previous_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({'left': left});
+            previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+        }, 
+        duration: 800, 
+        complete: function(){
+            current_fs.hide();
+            animating = false;
+        }, 
+        //this comes from the custom easing plugin
+        easing: 'easeInOutBack'
+    });
+});
+
+// add btn and delete
+
+$(function() {
+    $('#myFormset1 .formset-list1 .formset1').formset({
+        prefix: '{{ experienceForm.prefix }}',
+        formCssClass: 'formset1',
+        addCssClass: 'btn btn-success btn-sm add-form-row1 w-100 p-12',
+        deleteCssClass:'btn btn-danger btn-sm delete-form-row1 w-100 p-12',
+        addText:'+ Add'
+    });
+
+    $('#myFormset2 .formset-list2 .formset2').formset({
+        prefix: '{{ educationForm.prefix }}',
+        formCssClass: 'formset2',
+        addCssClass: 'btn btn-success btn-sm add-form-row1 w-100 p-12',
+        deleteCssClass:'btn btn-danger btn-sm delete-form-row1 w-100 p-12',
+        addText:'+ Add'
+
+    });
+
+    $('#myFormset3 .formset-list3 .formset3').formset({
+        prefix: '{{ skillForm.prefix }}',
+        formCssClass: 'formset3',
+        addCssClass: 'btn btn-success btn-sm add-form-row1 w-100 p-12',
+        deleteCssClass:'btn btn-danger btn-sm delete-form-row1 w-100 p-12',
+        addText:'+ Add'
+
+    });
+
+})
+
+
+    
+
+
+
+});
