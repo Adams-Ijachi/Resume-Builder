e5b1e689ed66ce0965446539e146e48ab7b6276b
Adams Ijachi <adamsijachi9@gmail.com>
2020-12-14 23:32:45 +0100

    env

--- core/settings.py
+++ core/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fiq^blz@f!lpf3z@3)@l9j49m1j52txtlyk3gs0k_d*97u^ar%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','resumemaker12.herokuapp.com']
 
 
 # Application definition
